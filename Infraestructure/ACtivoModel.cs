6193a072bf5c1ec6151bf4c0e432f5c069db80d9
MoisesNunez435 <85852193+MoisesNunez435@users.noreply.github.com>
2021-09-28 11:30:12 -0600

    Add files via upload

---
+++ Infraestructure/ACtivoModel.cs
@@ -0,0 +1,39 @@
+﻿using DoMain;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infraestructure
+{
+    public class ActivoModel
+    {
+        public ActivoFijo[] activos;
+
+        public void Add(ActivoFijo af, ref ActivoFijo[] acts)
+        {
+            if(acts == null)
+            {
+                acts = new ActivoFijo[1];
+                acts[acts.Length - 1] = af;
+                return;
+            }
+
+            ActivoFijo[] tmp = new ActivoFijo[acts.Length + 1];
+            Array.Copy(acts, tmp, acts.Length);
+            tmp[tmp.Length - 1] = af;
+            acts = tmp;
+        }
+
+        //public decimal CalcularDprLineaRecta(ActivoFijo e)
+        //{
+        //    decimal[] TotalDpr = new decimal[];
+
+        //}
+
+        public string GetActiveAsJason()
+        {
+            return JsonConvert.SerializeObject(activos);
+        }
+    }
+}
